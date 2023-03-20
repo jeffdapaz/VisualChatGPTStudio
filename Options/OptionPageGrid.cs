6f246c0ac4288969940c4d2d5be254e321dadbdb
Graham Glover <graham.glover@draftable.com>
2023-03-20 11:31:24 +1100

    Added stop sequence option and upgraded openai wrapper nuget

--- Options/OptionPageGrid.cs
+++ Options/OptionPageGrid.cs
@@ -55,6 +55,12 @@ namespace JeffPires.VisualChatGPTStudio.Options
         [DefaultValue(0)]
         [TypeConverter(typeof(DoubleConverter))]
         public double TopP { get; set; } = 0;
+
+        [Category("Visual chatGPT Studio")]
+        [DisplayName("stop sequence")]
+        [Description("Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence. Separate different stop strings by a comma e.g. '},;,stop'")]
+        [DefaultValue("")]
+        public string StopSeqeuences{ get; set; } = string.Empty;
     }
 
     /// <summary>

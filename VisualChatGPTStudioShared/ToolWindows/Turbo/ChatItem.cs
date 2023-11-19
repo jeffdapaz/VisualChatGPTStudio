92765e43e88f6045e4dc4872504bddbd1ddd3cbb
Jefferson Pires <j.da.paz.pires@avanade.com>
2023-11-19 23:33:58 +0000

    Added scroll to the Turbo Chat Tabs

--- VisualChatGPTStudioShared/ToolWindows/Turbo/ChatItem.cs
+++ VisualChatGPTStudioShared/ToolWindows/Turbo/ChatItem.cs
@@ -1,17 +1,29 @@
-﻿using System.Windows.Controls;
-
-namespace JeffPires.VisualChatGPTStudio.ToolWindows.Turbo
-{
-    internal class ChatItem
-    {
+﻿using System.Windows.Controls;namespace JeffPires.VisualChatGPTStudio.ToolWindows.Turbo{
+    /// <summary>
+    /// Represents a chat item in the Turbo Chat.
+    /// </summary>
+    internal class ChatItem    {
+        /// <summary>
+        /// Gets or sets the Id of the chat.
+        /// </summary>
         public string Id { get; set; }
 
-        public string Name { get; set; }        
+        /// <summary>
+        /// Gets or sets the name of the chat.
+        /// </summary>
+        public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the chat header user control.
+        /// </summary>
         public ucChatHeader Header { get; set; }
 
+        /// <summary>
+        /// Gets or sets the TabItem associated with this chat.
+        /// </summary>
         public TabItem TabItem { get; set; }
 
-        public ucChatItem ListItem { get; set; }
-    }
-}
+        /// <summary>
+        /// Gets or sets the ucChatItem object representing a chat item in the chat list.
+        /// </summary>
+        public ucChatItem ListItem { get; set; }    }}
